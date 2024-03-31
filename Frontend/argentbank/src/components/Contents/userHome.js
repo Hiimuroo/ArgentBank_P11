b9f82acd80ddfd65b8085c9aac3eb370b7844f22
Hiimuroo <matthieu.labille@gmail.com>
2024-03-31 18:38:38 +0200

    modified:   Frontend/argentbank/src/components/Contents/userHome.js

--- Frontend/argentbank/src/components/Contents/userHome.js
+++ Frontend/argentbank/src/components/Contents/userHome.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Account from './comptes';
+import { useSelector } from 'react-redux';
 
 const UserHome = () => {
+  const { firstName, lastName } = useSelector((state) => state.user); // Affichage Prenom et Nom du client page accueil (userName si on veut afficher seulement son pseudo)
 
   return (
     <main className="main bg-dark">
       <div className="header">
         <h1>Welcome back </h1>
+        <h1> {firstName} {lastName} !</h1>
       </div>
       <h2 className="sr-only">Accounts</h2>
       <Account title="Argent Bank Checking (x8349)" amount="$2,082.79" description="Available Balance" />
